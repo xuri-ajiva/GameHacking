893393e46aeed54ef66865b2e51f04fd85f18c25
xuri02 <T0Bl4S.durst@gmail.com>
2020-04-09 20:39:51 +0200

    new 3d sven coop map

---
+++ Sven-Coop-Map/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml.Serialization;
+using HackFramework;
+using HelixToolkit.Wpf;
+using Microsoft.Win32;
+using Path = System.IO.Path;
+
+namespace Sven_Coop_Map {
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        public struct Point3DPlus {
+            public Point3DPlus(Point3D point, Color color, double thickness) {
+                this.point     = point;
+                this.color     = color;
+                this.thickness = thickness;
+            }
+
+            public Point3D point;
+            public Color   color;
+            public double  thickness;
+        }
+
+        private List<Point3DPlus> points    = new List<Point3DPlus>();
+        private List<Point3DPlus> totalData = new List<Point3DPlus>();
+        private Stopwatch         stopwatch = Stopwatch.StartNew();
+
+        public MainWindow() {
+            InitializeComponent();
+
+            var bw = new BackgroundWorker();
+            bw.DoWork += GatherData;
+            bw.RunWorkerAsync();
+        }
+
+        private void GatherData(object sender, DoWorkEventArgs e) {
+            while ( true ) {
+                Thread.Sleep( 50 ); // 50ms data sampling period
+
+                // Generate a test trace: an upward spiral with square corners
+                double t   = this.stopwatch.Elapsed.TotalSeconds * 0.25;
+                var    pos = Vec3.Zero;
+                //double x, y, z = t * 0.5;
+                Color color = default;
+
+                ReadFromGame( ref pos, ref color, t );
+
+                var point  = new Point3DPlus( new Point3D( pos.x, pos.y, pos.z ), color, 1.5 );
+                var invoke = false;
+
+                lock (this.points) {
+                    this.points.Add( point );
+                    invoke = ( this.points.Count == 1 );
+                }
+
+                if ( invoke ) this.Dispatcher.BeginInvoke( DispatcherPriority.Background, (Action) PlotData );
+            }
+        }
+
+        private void ReadFromGame(ref Vec3 pos, ref Color color, double t) {
+            color = MkColor( t / 10d );
+            pos   = Program.CalcPos();
+        }
+
+        static Color MkColor(double t) { return Color.FromRgb( (byte) ( ( Math.Sin( t ) + 1 ) * 127 ), (byte) ( ( Math.Cos( t ) + 1 ) * 127 ), (byte) ( ( -Math.Sin( t ) + 1 ) * 127 ) ); }
+
+        private void PlotData() {
+            if ( this.points.Count == 1 ) {
+                Point3DPlus point;
+
+                lock (this.points) {
+                    point = this.points[0];
+                    this.points.Clear();
+                }
+
+                this.totalData.Add( point );
+                this.plot.AddPoint( point.point, point.color, point.thickness );
+            }
+            else {
+                Point3DPlus[] pointsArray;
+
+                lock (this.points) {
+                    pointsArray = this.points.ToArray();
+                    this.points.Clear();
+                }
+
+                this.totalData.AddRange( pointsArray );
+                foreach ( Point3DPlus point in pointsArray ) this.plot.AddPoint( point.point, point.color, point.thickness );
+            }
+        }
+
+        private void btnZoom_Click(object sender, RoutedEventArgs e) {
+            this.plot.ZoomExtents( 500 ); // zoom to extents
+            //plot.ResetCamera();  // orient and zoom
+        }
+
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e) {
+            var item = (MenuItem) sender;
+
+            switch (item.TabIndex) {
+                case 1:
+                    this.plot.Clear();
+                    break;
+                case 2:
+                    try {
+                        var s = new SaveFileDialog { Filter = "*.XML|*.XML" };
+
+                        if ( s.ShowDialog( this ) == true ) {
+                            var x  = new XmlSerializer( this.totalData.GetType() );
+                            var fs = File.Open( Path.GetDirectoryName( s.FileName ) + "\\" + Path.GetFileNameWithoutExtension( s.FileName ) + ".SavedMap" + Path.GetExtension( s.FileName ), FileMode.OpenOrCreate );
+                            x.Serialize( fs, this.totalData );
+                            fs.Close();
+                            x = null;
+
+                            MessageBox.Show( "Saving succeeded!" );
+                        }
+                    } catch (Exception ex) {
+                        MessageBox.Show( ex.ToString() );
+                    }
+
+                    break;
+                case 3:
+                    try {
+                        var o = new OpenFileDialog { Filter = "*.XML|*.XML" };
+
+                        if ( o.ShowDialog( this ) == true ) {
+                            var x  = new XmlSerializer( this.totalData.GetType() );
+                            var fs = File.Open( o.FileName, FileMode.Open );
+
+                            lock (this.points) {
+                                this.points.AddRange( (List<Point3DPlus>) x.Deserialize( fs ) );
+                            }
+
+                            fs.Close();
+                            x = null;
+                            PlotData();
+                            MessageBox.Show( "Loading succeeded!" );
+                        }
+                    } catch (Exception ex) {
+                        MessageBox.Show( ex.ToString() );
+                    }
+
+                    break;
+                case 99:
+                    Program.Position.RecalculatePtr();
+                    break;
+            }
+        }
+    }
+
+}
---
+++ Sven-Coop-Map/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HackFramework;
+
+namespace Sven_Coop_Map {
+    public static class Program {
+        public static   Memory   M;
+        static readonly IntPtr[] Offsets = { (IntPtr) 0x570EE00, (IntPtr) 0x88 };
+        private const   string   PROCESS = "svencoop";
+
+
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        public static void Main(string[] args) {
+            M = new Memory();
+            M.AttachLoop( PROCESS );
+            
+            Position = new DirectUpdate<Vec3>(M, Offsets, "hw.dll" );
+
+            new Thread( () => {
+                const int sleep  = 1;
+                const int radius = 200;
+                Thread.Sleep( 2000 );
+                var    sp   = Position.Value;
+                double time = 0;
+
+                while ( true ) {
+                    var cp = sp.Add( new Vec3( Math.Sin( time * ( Math.E - 2.6 ) ) * radius, Math.Cos( time * ( Math.PI - 3 ) ) * radius, -Math.Sin( time * ( Math.E + Math.PI - 5.7 ) ) * radius ) );
+                    Position.Value = cp;
+                    Thread.Sleep( sleep );
+                    time += sleep / 100d;
+                }
+            } );//.Start();
+
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+            Thread.Sleep( 1000 );
+            Environment.Exit( 0 );
+        }
+
+        public static DirectUpdate<Vec3> Position;
+
+        private const int SIZE   = 1000;
+        private const int FACTOR = 100;
+        private const int PS     = 5;
+
+        public static Vec3 CalcPos() {
+            var pos = Position.Value;
+
+            float x = pos.X / ( SIZE / FACTOR );
+            float y = pos.Y / ( SIZE / FACTOR );
+            float z = pos.Z / ( SIZE / FACTOR );
+            return new Vec3( x, y, z );
+        }
+    }
+}
